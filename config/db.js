8e57b4ff25973013b03c0d16b9c401cdde4d5477
Rehan Asghar <rehan79664703@gmail.com>
2026-06-05 01:18:16 +0500

    update mongodb url

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ dotenv.config()
 
 export const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_LOCAL_URI);
+    const conn = await mongoose.connect(process.env.MONGO_URL);
     if (conn) {
       console.log(`mongodb connected at ${conn.connection.host}`);
     }
