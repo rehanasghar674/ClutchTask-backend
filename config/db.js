379f9567fb40281dd920ac408fafd987446a9953
Rehan Asghar <rehan79664703@gmail.com>
2026-06-05 03:47:00 +0500

    clutchtask-backend

--- config/db.js
+++ config/db.js
@@ -4,7 +4,10 @@ dotenv.config()
 
 export const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URL);
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     if (conn) {
       console.log(`mongodb connected at ${conn.connection.host}`);
     }
