8cdc15fe59a5fb88b8e1f094e4184d23e1deccd7
Rehan Asghar <rehan79664703@gmail.com>
2026-06-05 04:48:15 +0500

    update data

--- index.js
+++ index.js
@@ -9,7 +9,10 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://clutch-task-frontend.vercel.app',
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
